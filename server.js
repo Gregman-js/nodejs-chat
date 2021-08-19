b7904f9a8af7b8801778ed773f4249b35c5f3a36
Grzegorz Libiszewski <grzegorz.libiszewski7@gmail.com>
2021-08-19 22:20:35 +0200

    Update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var server = require('http').createServer(app);
-var io = require("socket.io").listen(server);
+var io = require("socket.io")(server);
 var fs = require("fs");
 var pamiec;
 var users = [];
